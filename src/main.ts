5109a25d5691e2a37d6a1e3d726d7c8f06c46d66
KhoaPiX <lukhoa2309@gmail.com>
2025-05-12 09:58:36 +0700

    Thêm 1 số api và cài swagger rồi đó

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,13 @@ async function bootstrap() {
 
   .build();
   const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, 
+      hashPrefix: '',
+    },
+  });
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
