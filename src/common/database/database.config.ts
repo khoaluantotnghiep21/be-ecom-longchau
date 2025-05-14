a58896692dc1080c7e957c78db83a329d8f693d2
kimphuongnt <kimphuongnt.dev@gmail.com>
2025-05-15 00:31:50 +0700

    lần nữa

--- src/common/database/database.config.ts
+++ src/common/database/database.config.ts
@@ -6,7 +6,7 @@ export const databaseConfig = (): SequelizeModuleOptions => ({
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   autoLoadModels: true,
   synchronize: false,
 });
