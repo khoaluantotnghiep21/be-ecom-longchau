4b991a9eb68ee9d829c5ca7599c24b952f70965b
KhoaPiX <lukhoa2309@gmail.com>
2025-05-17 13:29:12 +0700

    update identity user 2

--- src/Module/IdentityUser/dto/signIn.dto.ts
+++ src/Module/IdentityUser/dto/signIn.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
 
 export class SignInDto {
   @ApiProperty()
+  @IsPhoneNumber()
+  @IsNotEmpty()
   sodienthoai: string;
+  @IsNotEmpty()
   @ApiProperty()
   matkhau: string;
 }
