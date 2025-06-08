42251997b5c188daf2ea9e747c97a1327153adf6
Minh Tâm <nguyenminhtam01632.fetel@gmail.com>
2025-06-08 14:27:34 +0700

    update

--- src/Module/DonHang/dto/createPaymen.dto.ts
+++ src/Module/DonHang/dto/createPaymen.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsNotEmpty } from "class-validator";
 
 export class CreatePaymentDto{
     @ApiProperty()
     amount: number;
     @ApiProperty()
     madonhang: string;
+    
+    @IsNotEmpty()
+    @IsEnum(['web', 'mobile'], { message: 'Platform must be either web or mobile' })
+    platform: 'web' | 'mobile'; // thêm dòng này
 }
